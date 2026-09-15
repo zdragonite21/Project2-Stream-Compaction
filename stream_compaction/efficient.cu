1f5d16888a617d865a4ee33273194f7ee2ac522b
zdragonite21 <zdragonite21@gmail.com>
2026-09-15 18:04:25 -0400

    passed extended to larger block sizes, of arbitrary length

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -14,23 +14,37 @@ PerformanceTimer &timer() {
     return timer;
 }
 
-__global__ void kern_scan(int n, int *data) {
+__global__ void kern_inc(int n, int data_num_blocks, int *data, int *sums) {
+    // a thread is spawned per block
+    int thid = blockIdx.x * blockDim.x + threadIdx.x;
+    if (thid >= data_num_blocks) {
+        return;
+    }
+    int base = thid * n;
+    int inc = sums[thid];
+    for (int i = 0; i < n; ++i) {
+        data[base + i] += inc;
+    }
+}
+
+__global__ void kern_scan(int n, int *data, int *sums, bool write_sum) {
     extern __shared__ int temp[];
 
-    int thid = blockIdx.x * blockDim.x + threadIdx.x;
+    int local_thid = threadIdx.x;
+    int block_base = blockIdx.x * n;
     // if (thid >= n) {
     //     return;
     // }
 
-    temp[thid << 1] = data[thid << 1];
-    temp[(thid << 1) + 1] = data[(thid << 1) + 1];
+    temp[local_thid << 1] = data[block_base + (local_thid << 1)];
+    temp[(local_thid << 1) + 1] = data[block_base + (local_thid << 1) + 1];
 
     // upsweep
     int offset = 1;
     for (int d = n >> 1; d > 0; d >>= 1) {
         __syncthreads();
-        if (thid < d) {
-            int base = 2 * offset * thid;
+        if (local_thid < d) {
+            int base = 2 * offset * local_thid;
             int ai = base + offset - 1;
             int bi = base + 2 * offset - 1;
             temp[bi] += temp[ai];
@@ -39,7 +53,8 @@ __global__ void kern_scan(int n, int *data) {
     }
 
     // downsweep
-    if (thid == 0) {
+    int total = temp[n - 1];
+    if (local_thid == 0) {
         // have the first thread clear the last element
         temp[n - 1] = 0;
     }
@@ -47,8 +62,8 @@ __global__ void kern_scan(int n, int *data) {
     for (int d = 1; d < n; d <<= 1) {
         offset >>= 1;
         __syncthreads();
-        if (thid < d) {
-            int base = 2 * offset * thid;
+        if (local_thid < d) {
+            int base = 2 * offset * local_thid;
             int ai = base + offset - 1;
             int bi = base + 2 * offset - 1;
 
@@ -59,36 +74,60 @@ __global__ void kern_scan(int n, int *data) {
     }
 
     __syncthreads();
-    data[thid << 1] = temp[thid << 1];
-    data[(thid << 1) + 1] = temp[(thid << 1) + 1];
+    data[block_base + (local_thid << 1)] = temp[local_thid << 1];
+    data[block_base + (local_thid << 1) + 1] = temp[(local_thid << 1) + 1];
+
+    if (write_sum && local_thid == 0) {
+        sums[blockIdx.x] = total;
+    }
 }
 
 /**
  * Performs prefix-sum (aka scan) on idata, storing the result into odata.
  */
 void scan(int n, int *odata, const int *idata) {
-    int size = (block_size << 1);
-    int offset = size - n;
+    int size_per_block = (block_size << 1);
+
+    int data_num_blocks = divup(n, size_per_block);
+    int data_pad = size_per_block - (n % size_per_block);
+    int data_total_size = data_num_blocks * size_per_block;
 
     int *dev_data;
-    cudaMalloc((void **)&dev_data, size * sizeof(int));
+    cudaMalloc((void **)&dev_data, data_total_size * sizeof(int));
     checkCUDAError("cudaMalloc dev_data failed!");
-    cudaMemset(dev_data, 0, offset * sizeof(int));
-    cudaMemcpy(dev_data + offset, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemset(dev_data, 0, data_pad * sizeof(int));
+    checkCUDAError("cudaMemset dev_data failed!");
+    cudaMemcpy(dev_data + data_pad, idata, n * sizeof(int),
+               cudaMemcpyHostToDevice);
     checkCUDAError("cudaMemcpy dev_data failed!");
 
+    int sum_num_blocks = divup(data_num_blocks, size_per_block);
+    int sum_pad = size_per_block - (data_num_blocks % size_per_block);
+    int sum_total_size = sum_num_blocks * size_per_block;
+
+    int *dev_sums;
+    cudaMalloc((void **)&dev_sums, sum_total_size * sizeof(int));
+    checkCUDAError("cudaMalloc dev_block_sums failed!");
+    cudaMemset(dev_sums, 0, sum_pad * sizeof(int));
+    checkCUDAError("cudaMemset dev_block_sums failed!");
+
     timer().startGpuTimer();
-    int num_blocks = divup(n, size);
-    int depth = ilog2ceil(n);
-    // each block handles a range of 2 * block_size elements
-    kern_scan<<<num_blocks, block_size, size * sizeof(int)>>>(
-        size, dev_data);
+    // int depth = ilog2ceil(n);
+    kern_scan<<<data_num_blocks, block_size, size_per_block * sizeof(int)>>>(
+        size_per_block, dev_data, dev_sums + sum_pad, true);
+
+    kern_scan<<<sum_num_blocks, block_size, size_per_block * sizeof(int)>>>(
+        size_per_block, dev_sums, nullptr, false);
+
+    kern_inc<<<sum_num_blocks, block_size>>>(size_per_block, data_num_blocks, dev_data,
+                                             dev_sums);
 
     timer().endGpuTimer();
 
-    cudaMemcpy(odata, dev_data + offset, n * sizeof(int), cudaMemcpyDeviceToHost);
-
+    cudaMemcpy(odata, dev_data + data_pad, n * sizeof(int),
+               cudaMemcpyDeviceToHost);
     cudaFree(dev_data);
+    cudaFree(dev_sums);
 }
 
 /**
