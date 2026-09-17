0c5d3dd2afff748696f5d4309951c2ac3d1bf710
zdragonite21 <zdragonite21@gmail.com>
2026-09-17 15:58:25 -0400

    profiling setup and old recursive code

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,34 @@
  * @copyright University of Pennsylvania
  */
 
-#include <cstdio>
-#include <stream_compaction/cpu.h>
-#include <stream_compaction/naive.h>
-#include <stream_compaction/efficient.h>
-#include <stream_compaction/thrust.h>
 #include "testing_helpers.hpp"
+#include <array>
+#include <cstdio>
+#include <functional>
+#include <stream_compaction/chunk.h>
+#include <stream_compaction/cpu.h>
+#include <stream_compaction/efficient.h>
+#include <stream_compaction/naive.h>
+#include <stream_compaction/thrust.h>
+#include <string>
+#include <iostream>
 
-const int SIZE = 1 << 25; // feel free to change the size of array
+#define PROFILE 1
+
+const int SIZE = 1 << 25;  // feel free to change the size of array
 const int NPOT = SIZE - 3; // Non-Power-Of-Two
 int *a = new int[SIZE];
 int *b = new int[SIZE];
 int *c = new int[SIZE];
 
-int main(int argc, char* argv[]) {
+void profile();
+
+int main(int argc, char *argv[]) {
+
+#if PROFILE
+    profile();
+    return 0;
+#endif
     // Scan tests
 
     printf("\n");
@@ -27,72 +41,88 @@ int main(int argc, char* argv[]) {
     printf("** SCAN TESTS **\n");
     printf("****************\n");
 
-    genArray(SIZE - 1, a, 50);  // Leave a 0 at the end to test that edge case
+    genArray(SIZE - 1, a, 50); // Leave a 0 at the end to test that edge case
     a[SIZE - 1] = 0;
     printArray(SIZE, a, true);
 
     // initialize b using StreamCompaction::CPU::scan you implement
-    // We use b for further comparison. Make sure your StreamCompaction::CPU::scan is correct.
-    // At first all cases passed because b && c are all zeroes.
+    // We use b for further comparison. Make sure your
+    // StreamCompaction::CPU::scan is correct. At first all cases passed because
+    // b && c are all zeroes.
     zeroArray(SIZE, b);
     printDesc("cpu scan, power-of-two");
     StreamCompaction::CPU::scan(SIZE, b, a);
-    printElapsedTime(StreamCompaction::CPU::timer().getCpuElapsedTimeForPreviousOperation(), "(std::chrono Measured)");
+    printElapsedTime(
+        StreamCompaction::CPU::timer().getCpuElapsedTimeForPreviousOperation(),
+        "(std::chrono Measured)");
     printArray(SIZE, b, true);
 
     zeroArray(SIZE, c);
     printDesc("cpu scan, non-power-of-two");
     StreamCompaction::CPU::scan(NPOT, c, a);
-    printElapsedTime(StreamCompaction::CPU::timer().getCpuElapsedTimeForPreviousOperation(), "(std::chrono Measured)");
+    printElapsedTime(
+        StreamCompaction::CPU::timer().getCpuElapsedTimeForPreviousOperation(),
+        "(std::chrono Measured)");
     printArray(NPOT, c, true);
     printCmpResult(NPOT, b, c);
 
     zeroArray(SIZE, c);
     printDesc("naive scan, power-of-two");
     StreamCompaction::Naive::scan(SIZE, c, a);
-    printElapsedTime(StreamCompaction::Naive::timer().getGpuElapsedTimeForPreviousOperation(), "(CUDA Measured)");
-    //printArray(SIZE, c, true);
+    printElapsedTime(StreamCompaction::Naive::timer()
+                         .getGpuElapsedTimeForPreviousOperation(),
+                     "(CUDA Measured)");
+    // printArray(SIZE, c, true);
     printCmpResult(SIZE, b, c);
 
-    /* For bug-finding only: Array of 1s to help find bugs in stream compaction or scan
-    onesArray(SIZE, c);
-    printDesc("1s array for finding bugs");
+    /* For bug-finding only: Array of 1s to help find bugs in stream compaction
+    or scan onesArray(SIZE, c); printDesc("1s array for finding bugs");
     StreamCompaction::Naive::scan(SIZE, c, a);
     printArray(SIZE, c, true); */
 
     zeroArray(SIZE, c);
     printDesc("naive scan, non-power-of-two");
     StreamCompaction::Naive::scan(NPOT, c, a);
-    printElapsedTime(StreamCompaction::Naive::timer().getGpuElapsedTimeForPreviousOperation(), "(CUDA Measured)");
-    //printArray(SIZE, c, true);
+    printElapsedTime(StreamCompaction::Naive::timer()
+                         .getGpuElapsedTimeForPreviousOperation(),
+                     "(CUDA Measured)");
+    // printArray(SIZE, c, true);
     printCmpResult(NPOT, b, c);
 
     zeroArray(SIZE, c);
     printDesc("work-efficient scan, power-of-two");
     StreamCompaction::Efficient::scan(SIZE, c, a);
-    printElapsedTime(StreamCompaction::Efficient::timer().getGpuElapsedTimeForPreviousOperation(), "(CUDA Measured)");
-    //printArray(SIZE, c, true);
+    printElapsedTime(StreamCompaction::Efficient::timer()
+                         .getGpuElapsedTimeForPreviousOperation(),
+                     "(CUDA Measured)");
+    // printArray(SIZE, c, true);
     printCmpResult(SIZE, b, c);
 
     zeroArray(SIZE, c);
     printDesc("work-efficient scan, non-power-of-two");
     StreamCompaction::Efficient::scan(NPOT, c, a);
-    printElapsedTime(StreamCompaction::Efficient::timer().getGpuElapsedTimeForPreviousOperation(), "(CUDA Measured)");
-    //printArray(NPOT, c, true);
+    printElapsedTime(StreamCompaction::Efficient::timer()
+                         .getGpuElapsedTimeForPreviousOperation(),
+                     "(CUDA Measured)");
+    // printArray(NPOT, c, true);
     printCmpResult(NPOT, b, c);
 
     zeroArray(SIZE, c);
     printDesc("thrust scan, power-of-two");
     StreamCompaction::Thrust::scan(SIZE, c, a);
-    printElapsedTime(StreamCompaction::Thrust::timer().getGpuElapsedTimeForPreviousOperation(), "(CUDA Measured)");
-    //printArray(SIZE, c, true);
+    printElapsedTime(StreamCompaction::Thrust::timer()
+                         .getGpuElapsedTimeForPreviousOperation(),
+                     "(CUDA Measured)");
+    // printArray(SIZE, c, true);
     printCmpResult(SIZE, b, c);
 
     zeroArray(SIZE, c);
     printDesc("thrust scan, non-power-of-two");
     StreamCompaction::Thrust::scan(NPOT, c, a);
-    printElapsedTime(StreamCompaction::Thrust::timer().getGpuElapsedTimeForPreviousOperation(), "(CUDA Measured)");
-    //printArray(NPOT, c, true);
+    printElapsedTime(StreamCompaction::Thrust::timer()
+                         .getGpuElapsedTimeForPreviousOperation(),
+                     "(CUDA Measured)");
+    // printArray(NPOT, c, true);
     printCmpResult(NPOT, b, c);
 
     printf("\n");
@@ -102,18 +132,21 @@ int main(int argc, char* argv[]) {
 
     // Compaction tests
 
-    genArray(SIZE - 1, a, 4);  // Leave a 0 at the end to test that edge case
+    genArray(SIZE - 1, a, 4); // Leave a 0 at the end to test that edge case
     a[SIZE - 1] = 0;
     printArray(SIZE, a, true);
 
     int count, expectedCount, expectedNPOT;
 
-    // initialize b using StreamCompaction::CPU::compactWithoutScan you implement
-    // We use b for further comparison. Make sure your StreamCompaction::CPU::compactWithoutScan is correct.
+    // initialize b using StreamCompaction::CPU::compactWithoutScan you
+    // implement We use b for further comparison. Make sure your
+    // StreamCompaction::CPU::compactWithoutScan is correct.
     zeroArray(SIZE, b);
     printDesc("cpu compact without scan, power-of-two");
     count = StreamCompaction::CPU::compactWithoutScan(SIZE, b, a);
-    printElapsedTime(StreamCompaction::CPU::timer().getCpuElapsedTimeForPreviousOperation(), "(std::chrono Measured)");
+    printElapsedTime(
+        StreamCompaction::CPU::timer().getCpuElapsedTimeForPreviousOperation(),
+        "(std::chrono Measured)");
     expectedCount = count;
     printArray(count, b, true);
     printCmpLenResult(count, expectedCount, b, b);
@@ -121,7 +154,9 @@ int main(int argc, char* argv[]) {
     zeroArray(SIZE, c);
     printDesc("cpu compact without scan, non-power-of-two");
     count = StreamCompaction::CPU::compactWithoutScan(NPOT, c, a);
-    printElapsedTime(StreamCompaction::CPU::timer().getCpuElapsedTimeForPreviousOperation(), "(std::chrono Measured)");
+    printElapsedTime(
+        StreamCompaction::CPU::timer().getCpuElapsedTimeForPreviousOperation(),
+        "(std::chrono Measured)");
     expectedNPOT = count;
     printArray(count, c, true);
     printCmpLenResult(count, expectedNPOT, b, c);
@@ -129,25 +164,141 @@ int main(int argc, char* argv[]) {
     zeroArray(SIZE, c);
     printDesc("cpu compact with scan");
     count = StreamCompaction::CPU::compactWithScan(SIZE, c, a);
-    printElapsedTime(StreamCompaction::CPU::timer().getCpuElapsedTimeForPreviousOperation(), "(std::chrono Measured)");
+    printElapsedTime(
+        StreamCompaction::CPU::timer().getCpuElapsedTimeForPreviousOperation(),
+        "(std::chrono Measured)");
     printArray(count, c, true);
     printCmpLenResult(count, expectedCount, b, c);
 
     zeroArray(SIZE, c);
     printDesc("work-efficient compact, power-of-two");
     count = StreamCompaction::Efficient::compact(SIZE, c, a);
-    printElapsedTime(StreamCompaction::Efficient::timer().getGpuElapsedTimeForPreviousOperation(), "(CUDA Measured)");
-    //printArray(count, c, true);
+    printElapsedTime(StreamCompaction::Efficient::timer()
+                         .getGpuElapsedTimeForPreviousOperation(),
+                     "(CUDA Measured)");
+    // printArray(count, c, true);
     printCmpLenResult(count, expectedCount, b, c);
 
     zeroArray(SIZE, c);
     printDesc("work-efficient compact, non-power-of-two");
     count = StreamCompaction::Efficient::compact(NPOT, c, a);
-    printElapsedTime(StreamCompaction::Efficient::timer().getGpuElapsedTimeForPreviousOperation(), "(CUDA Measured)");
-    //printArray(count, c, true);
+    printElapsedTime(StreamCompaction::Efficient::timer()
+                         .getGpuElapsedTimeForPreviousOperation(),
+                     "(CUDA Measured)");
+    // printArray(count, c, true);
     printCmpLenResult(count, expectedNPOT, b, c);
 
     system("pause"); // stop Win32 console from closing on exit
+    delete[] a;
+    delete[] b;
+    delete[] c;
+}
+
+struct ScanImpl {
+    std::function<void(int, int *, const int *)> scan;
+    std::function<float(void)> elapsed;
+    std::string name;
+};
+
+void profile() {
+    constexpr int runs = 10;
+
+    std::array<ScanImpl, 5> scan_funcs{
+        ScanImpl{StreamCompaction::CPU::scan,
+                 [] {
+                     return StreamCompaction::CPU::timer()
+                         .getCpuElapsedTimeForPreviousOperation();
+                 },
+                 "cpu scan"},
+
+        ScanImpl{StreamCompaction::Naive::scan,
+                 [] {
+                     return StreamCompaction::Naive::timer()
+                         .getGpuElapsedTimeForPreviousOperation();
+                 },
+                 "naive gpu scan"},
+        ScanImpl{StreamCompaction::Chunk::scan,
+                 [] {
+                     return StreamCompaction::Chunk::timer()
+                         .getGpuElapsedTimeForPreviousOperation();
+                 },
+                 "recursive shared memory chunk scan"},
+        ScanImpl{StreamCompaction::Efficient::scan,
+                 [] {
+                     return StreamCompaction::Efficient::timer()
+                         .getGpuElapsedTimeForPreviousOperation();
+                 },
+                 "efficient gpu scan"},
+        ScanImpl{StreamCompaction::Thrust::scan,
+                 [] {
+                     return StreamCompaction::Thrust::timer()
+                         .getGpuElapsedTimeForPreviousOperation();
+                 },
+                 "thrust scan"},
+    };
+
+    printf("\n");
+    printf("****************\n");
+    printf("** PROFILING **\n");
+    printf("****************\n");
+
+    std::cout << "runs = " << runs << std::endl;
+
+    genArray(SIZE - 1, a, 50);
+    a[SIZE - 1] = 0;
+    printArray(SIZE, a, true);
+
+    ScanImpl cpu = scan_funcs[0];
+    printf("*********** checking correctness... ***********\n");
+    zeroArray(SIZE, b);
+    cpu.scan(SIZE, b, a);
+
+    for (int i = 0; i < scan_funcs.size(); ++i) {
+        ScanImpl impl = scan_funcs[i];
+
+        zeroArray(SIZE, c);
+        printDesc((impl.name + ", power-of-two").c_str());
+        impl.scan(SIZE, c, a);
+        printElapsedTime(impl.elapsed(), "(ms)");
+        printCmpResult(SIZE, b, c);
+
+        zeroArray(SIZE, c);
+        printDesc((impl.name + ", non-power-of-two").c_str());
+        impl.scan(NPOT, c, a);
+        printElapsedTime(impl.elapsed(), "(ms)");
+        printCmpResult(NPOT, b, c);
+    }
+
+    printf("*********** profiling (POT) ***********\n");
+    for (int i = 0; i < scan_funcs.size(); ++i) {
+        ScanImpl impl = scan_funcs[i];
+        printDesc((impl.name + ", power-of-two").c_str());
+
+        float total_time{};
+
+        for (int j = 0; j < runs; ++j) {
+            zeroArray(SIZE, c);
+            impl.scan(SIZE, c, a);
+            total_time += impl.elapsed();
+        }
+        printElapsedTime(static_cast<float>(total_time / runs), "(avg ms)");
+    }
+
+    printf("*********** profiling (NON-POT) ***********\n");
+    for (int i = 0; i < scan_funcs.size(); ++i) {
+        ScanImpl impl = scan_funcs[i];
+        printDesc((impl.name + ", non-power-of-two").c_str());
+
+        float total_time{};
+
+        for (int j = 0; j < runs; ++j) {
+            zeroArray(SIZE, c);
+            impl.scan(NPOT, c, a);
+            total_time += impl.elapsed();
+        }
+        printElapsedTime(static_cast<float>(total_time / runs), "(avg ms)");
+    }
+
     delete[] a;
     delete[] b;
     delete[] c;
---
+++ stream_compaction/chunk.cu
@@ -0,0 +1,176 @@
+#include "common.h"
+#include "chunk.h"
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <iostream>
+
+#define threads_per_block 16
+
+namespace StreamCompaction {
+namespace Chunk {
+using StreamCompaction::Common::PerformanceTimer;
+PerformanceTimer &timer() {
+    static PerformanceTimer timer;
+    return timer;
+}
+
+__global__ void kern_inc(int chunk_size, int num_chunks, int *data, int *sums) {
+    // a thread is spawned per chunk
+    int thid = blockIdx.x * blockDim.x + threadIdx.x;
+    if (thid >= num_chunks) {
+        return;
+    }
+    int base = thid * chunk_size;
+    int inc = sums[thid];
+    for (int i = 0; i < chunk_size; ++i) {
+        data[base + i] += inc;
+    }
+}
+
+__global__ void kern_scan(int chunk_size, int *data, int *sums,
+                          bool store_sum) {
+    extern __shared__ int temp[];
+
+    int local_thid = threadIdx.x;
+    int block_base = blockIdx.x * chunk_size;
+
+    temp[local_thid << 1] = data[block_base + (local_thid << 1)];
+    temp[(local_thid << 1) + 1] = data[block_base + (local_thid << 1) + 1];
+
+    // upsweep
+    int offset = 1;
+    for (int d = chunk_size >> 1; d > 0; d >>= 1) {
+        __syncthreads();
+        if (local_thid < d) {
+            int base = 2 * offset * local_thid;
+            int ai = base + offset - 1;
+            int bi = base + 2 * offset - 1;
+            temp[bi] += temp[ai];
+        }
+        offset <<= 1;
+    }
+
+    // downsweep
+    int total;
+    if (local_thid == 0) {
+        // have the first thread clear the last element
+        total = temp[chunk_size - 1];
+        temp[chunk_size - 1] = 0;
+    }
+
+    for (int d = 1; d < chunk_size; d <<= 1) {
+        offset >>= 1;
+        __syncthreads();
+        if (local_thid < d) {
+            int base = 2 * offset * local_thid;
+            int ai = base + offset - 1;
+            int bi = base + 2 * offset - 1;
+
+            int t = temp[ai];
+            temp[ai] = temp[bi];
+            temp[bi] += t;
+        }
+    }
+
+    __syncthreads();
+    data[block_base + (local_thid << 1)] = temp[local_thid << 1];
+    data[block_base + (local_thid << 1) + 1] = temp[(local_thid << 1) + 1];
+
+    if (store_sum && local_thid == 0) {
+        sums[blockIdx.x] = total;
+    }
+}
+
+struct params {
+    int chunk_size;
+    int num_chunks;
+    int pad;
+    int padded_size;
+};
+
+params compute_params(int n) {
+    // one thread handles two elements
+    const int chunk_size = threads_per_block << 1;
+
+    int num_chunks = divup(n, chunk_size);
+    int pad = (chunk_size - (n % chunk_size)) % chunk_size;
+    int padded_size = num_chunks * chunk_size;
+
+    return params{
+        chunk_size,
+        num_chunks,
+        pad,
+        padded_size,
+    };
+}
+
+void recursive_scan(params p, int *dev_data) {
+    params sp = compute_params(p.num_chunks);
+
+    if (p.num_chunks > 1) {
+        int num_inc_blocks = divup(p.num_chunks, threads_per_block);
+
+        int *dev_sums;
+        cudaMalloc((void **)&dev_sums, sp.padded_size * sizeof(int));
+        checkCUDAError("cudaMalloc dev_block_sums failed!");
+        cudaMemset(dev_sums, 0, sp.pad * sizeof(int));
+        checkCUDAError("cudaMemset dev_block_sums failed!");
+
+        kern_scan<<<p.num_chunks, threads_per_block,
+                    p.chunk_size * sizeof(int)>>>(p.chunk_size, dev_data,
+                                                  dev_sums + sp.pad, true);
+        recursive_scan(sp, dev_sums);
+
+        kern_inc<<<num_inc_blocks, threads_per_block>>>(
+            p.chunk_size, p.num_chunks, dev_data, dev_sums + sp.pad);
+
+        cudaFree(dev_sums);
+    } else {
+        kern_scan<<<p.num_chunks, threads_per_block,
+                    p.chunk_size * sizeof(int)>>>(p.chunk_size, dev_data,
+                                                  nullptr, false);
+    }
+}
+
+/**
+ * Performs prefix-sum (aka scan) on idata, storing the result into odata.
+ */
+void scan(int n, int *odata, const int *idata) {
+    params p = compute_params(n);
+
+    // pad the front with zeros
+    int *dev_data;
+    cudaMalloc((void **)&dev_data, p.padded_size * sizeof(int));
+    checkCUDAError("cudaMalloc dev_data failed!");
+    cudaMemset(dev_data, 0, p.pad * sizeof(int));
+    checkCUDAError("cudaMemset dev_data failed!");
+    cudaMemcpy(dev_data + p.pad, idata, n * sizeof(int),
+               cudaMemcpyHostToDevice);
+    checkCUDAError("cudaMemcpy dev_data failed!");
+
+    timer().startGpuTimer();
+    recursive_scan(p, dev_data);
+    timer().endGpuTimer();
+
+    cudaMemcpy(odata, dev_data + p.pad, n * sizeof(int),
+               cudaMemcpyDeviceToHost);
+    cudaFree(dev_data);
+}
+
+/**
+ * Performs stream compaction on idata, storing the result into odata.
+ * All zeroes are discarded.
+ *
+ * @param n      The number of elements in idata.
+ * @param odata  The array into which to store elements.
+ * @param idata  The array of elements to compact.
+ * @returns      The number of elements remaining after compaction.
+ */
+int compact(int n, int *odata, const int *idata) {
+    timer().startGpuTimer();
+    // TODO
+    timer().endGpuTimer();
+    return -1;
+}
+} // namespace Efficient
+} // namespace StreamCompaction
